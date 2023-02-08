79b02e0b9a553c727defd7eec8eec0d194181637
Vajboy <122243706+Vajboy@users.noreply.github.com>
2023-02-08 13:20:07 +0530

    Add files via upload

---
+++ 1st question.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int number;
+	scanf("%d %d", &number1,number2);
+	printf("%d number1 is 5",number1);
+	printf("%d number1 is 3",number2);
+	if (number1.number2)
+	printf("%d is big",number1);
+	else
+	printf("%d is big",number2);
+}
---
+++ area compiler.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+main()
+{
+	float area,r,pi;
+	scanf ("%f",&r);
+	pi==22/7;
+	area==pi*r*r;
+	printf ("%f",area);
+}
